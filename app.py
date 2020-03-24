0fe2773ccca6a95cb5d253d7035d466ff28ac1b8
Jake Hassinger <jacob.r.hassinger@gmail.com>
2020-03-23 22:03:21 -0400

    updating app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import mission_to_mars
+import scrape_mars
 
 app = Flask(__name__)
 
@@ -17,9 +17,9 @@ def index():
 
 @app.route("/scrape")
 def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_craigslist.scrape()
-    listings.update({}, listings_data, upsert=True)
+    mars = mongo.db.mars_db
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
 
 
